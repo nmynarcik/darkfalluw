176666d559936b3e7bafaa6095b2964bfcb287b9
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-05 04:06:52 -0600

    added social icons to homepage

--- page-darkfall.php
+++ page-darkfall.php
@@ -24,7 +24,16 @@ $dir = get_bloginfo('url') . '/wp-content/plugins/df_content_info';
               echo $theContent;
             ?>
           <!-- </p> -->
-
+          <div class="df-social">
+          <h3>Social Media</h3>
+          <p>Stay up to date with Darkfall's social communities</p>
+          <ul class="social-list">
+            <li class="main"><a href="http://darkfallonline.com/uw/" alt="Darkfall Unholy Wars Official Website" title="Darkfall Unholy Wars Official Website" target="_blank">Main Site</a></li>
+            <li class="reddit"><a href="http://www.reddit.com/r/darkfall" alt="Darkfall Reddit" title="Darkfall on Reddit" target="_blank">Reddit</a></li>
+            <li class="twitter"><a href="https://twitter.com/darkfallmmorpg" alt="Darkfall on Twitter" title="Darkfall on Twitter" target="_blank">Twitter</a></li>
+            <li class="fb"><a href="http://www.facebook.com/darkfalluw" alt="Darkfall on Facebook" title="Darkfall on Facebook" target="_blank">Facebook</a></li>
+          </ul>
+        </div>
           <div class="clear"></div>
         </div>
         <?php endwhile; ?>
