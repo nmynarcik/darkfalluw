7f8e24beb7b5e9b100b783ccb5c5fd467e931d9f
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-07 14:16:33 -0600

    tweak map page

--- page-map.php
+++ page-map.php
@@ -15,7 +15,8 @@ wp_enqueue_script('dfuw_jquery','http://ajax.googleapis.com/ajax/libs/jquery/1.8
 <h1 class="entry-title"><?php the_title(); ?></h1>
 <div class="entry-content">
 <?php the_content(); ?>
-  <iframe src="http://www.geckzilla.com/dfstuff/dfmap.swf" width="695" height="695" ></iframe>
+<a href="http://www.geckzilla.com/dfstuff/dfmap.swf" class="btn btn-inverse pop-out" onclick="javascript:void window.open('http://www.geckzilla.com/dfstuff/dfmap.swf','1354867021522','width=1248,height=1024,toolbar=0,menubar=0,location=0,status=0,scrollbars=0,resizable=1,left=0,top=0');return false;">Popout <i class="icon-fullscreen icon-white"></i></a>
+  <iframe id="geck-map" src="http://www.geckzilla.com/dfstuff/dfmap.swf" width="695" height="695" ></iframe>
   <a href="http://www.geckzilla.com/dfstuff/dfmap.swf" class="btn btn-inverse pop-out" onclick="javascript:void window.open('http://www.geckzilla.com/dfstuff/dfmap.swf','1354867021522','width=1248,height=1024,toolbar=0,menubar=0,location=0,status=0,scrollbars=0,resizable=1,left=0,top=0');return false;">Popout <i class="icon-fullscreen icon-white"></i></a>
 </div>
 </div>
