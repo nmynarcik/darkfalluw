48f2d20ba41108875e02311fdbaa5ff5d3a66731
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-08 02:26:53 -0600

    google ads - MUAHAHAHAHHA. Closes #24

--- page-darkfall.php
+++ page-darkfall.php
@@ -8,6 +8,19 @@ wp_enqueue_script('dfuw_jquery','http://ajax.googleapis.com/ajax/libs/jquery/1.8
   <!-- CONTENT -->
     <div id="content" class="full-width">
       <section class="main-content full-height darkfall" role="main">
+        <div class="gad">
+          <script type="text/javascript"><!--
+          google_ad_client = "ca-pub-0373971494255887";
+          /* DFUW Info Site */
+          google_ad_slot = "4637838709";
+          google_ad_width = 728;
+          google_ad_height = 90;
+          //-->
+          </script>
+          <script type="text/javascript"
+          src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+          </script>
+        </div>
         <article>
         <?php while ( have_posts() ) : the_post(); ?>
         <header>
--- sidebar.php
+++ sidebar.php
@@ -1,4 +1,17 @@
 <aside id="sidebar">
+  <div class="gad">
+    <script type="text/javascript"><!--
+    google_ad_client = "ca-pub-0373971494255887";
+    /* DFUW Sidebar */
+    google_ad_slot = "6114571902";
+    google_ad_width = 180;
+    google_ad_height = 150;
+    //-->
+    </script>
+    <script type="text/javascript"
+    src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+    </script>
+  </div>
 <?php if ( is_active_sidebar('primary-widget-area') ) : ?>
 <div id="primary" class="widget-area">
 <ul class="sid">
