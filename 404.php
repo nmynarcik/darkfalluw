f4527d7a9f09cd0f5476c55c6a6b887b5b72c289
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-08 23:27:56 -0600

    we gots a 4 OH 4 baybay! closes #24

--- 404.php
+++ 404.php
@@ -1,12 +1,12 @@
 <?php get_header(); ?>
-<div id="content">
+<div id="content" class="full-width">
 <div id="post-0" class="post error404 not-found">
-<h1 class="entry-title"><?php _e('Not Found', 'darkfalluw'); ?></h1>
+<h1 class="entry-title"><?php _e('4&bull;OH&bull;4', 'darkfalluw'); ?></h1>
+<div class="df-404"><img src="<?php echo get_template_directory_uri(); ?>/images/4OH4.jpg" /></div>
 <div class="entry-content">
-<p><?php _e('Nothing found for the requested page. Try a search instead?', 'darkfalluw'); ?></p>
+<p><?php _e('Couldn\'t find anything at the page you requested. Try a search instead?', 'darkfalluw'); ?></p>
 <?php get_search_form(); ?>
 </div>
 </div>
 </div>
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
