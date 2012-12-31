424ef0851187858375eb7d889d8881aa850f1d47
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-31 03:04:14 -0600

    new map using google maps api

---
+++ js/agon.map.js
@@ -0,0 +1,37 @@
+function CustomMapType() {
+}
+CustomMapType.prototype.tileSize = new google.maps.Size(256,256);
+CustomMapType.prototype.maxZoom = 7;
+CustomMapType.prototype.getTile = function(coord, zoom, ownerDocument) {
+    var div = ownerDocument.createElement('DIV');
+    var baseURL = templateDir+'/images/tiles/';
+    baseURL += zoom + '_' + coord.x + '_' + coord.y + '.png';
+    div.style.width = this.tileSize.width + 'px';
+    div.style.height = this.tileSize.height + 'px';
+    div.style.backgroundColor = '#1B2D33';
+    div.style.backgroundImage = 'url(' + baseURL + ')';
+    return div;
+};
+
+CustomMapType.prototype.name = "Custom";
+CustomMapType.prototype.alt = "Tile Coordinate Map Type";
+var map;
+var CustomMapType = new CustomMapType();
+function map_initialize() {
+  var mapOptions = {
+      minZoom: 2,
+    maxZoom: 7,
+    isPng: true,
+      mapTypeControl: false,
+      streetViewControl: false,
+        center: new google.maps.LatLng(73,-109),
+      zoom: 3,
+    mapTypeControlOptions: {
+      mapTypeIds: ['custom', google.maps.MapTypeId.ROADMAP],
+      style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+    }
+  };
+map = new google.maps.Map(document.getElementById("map_canvas"),mapOptions);
+map.mapTypes.set('custom',CustomMapType);
+map.setMapTypeId('custom');
+}
