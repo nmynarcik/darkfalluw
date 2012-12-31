2e808dc0c992cae049ec16baa47d2832343bbab8
Nathan Mynarcik <nathan@mynarcik.com>
2012-12-31 16:38:11 -0600

    armor page added

---
+++ page-armor.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Template Name: Armor
+*/
+get_header();
+?>
+<article id="content" class="full-width">
+<?php the_post(); ?>
+<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<h1 class="entry-title"><?php the_title(); ?></h1>
+<div class="entry-content">
+  <?php the_content(); ?>
+  <iframe class="gdoc" width="100%" height="750" src="https://docs.google.com/spreadsheet/lv?key=0Aj1bEOdIK16ldEFPcnlKY3BCaW9IRGhSZlNmTWV4TFE&output=html"></iframe>
+  <div class="disclaimer">** Content provided by Kelet</div>
+</div>
+</div>
+</article>
+<?php get_footer(); ?>
