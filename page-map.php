b617de8aed0c90d153cca093402077462cd57da9
Nathan Mynarcik <nathan@mynarcik.com>
2013-01-04 18:34:59 -0600

    gotta make dat money, yo!

--- page-map.php
+++ page-map.php
@@ -7,13 +7,26 @@ get_header();
 wp_enqueue_script('dfuw_jquery','http://maps.googleapis.com/maps/api/js?libraries=places&sensor=false',false,'1.6','all');
 wp_enqueue_script('dfuw_map',get_template_directory_uri().'/js/agon.map.js',false,$ver,'all');
 ?>
-<article id="content" class='full-width'>
+<article id="content" class='full-width map'>
 <?php the_post(); ?>
 <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 <h1 class="entry-title"><?php the_title(); ?></h1>
 <div class="entry-content">
   <?php the_content(); ?>
   <div id="map-container">
+    <div class="gad">
+     <script type="text/javascript"><!--
+      google_ad_client = "ca-pub-0373971494255887";
+      /* SlimJim */
+      google_ad_slot = "3579150709";
+      google_ad_width = 728;
+      google_ad_height = 15;
+      //-->
+      </script>
+      <script type="text/javascript"
+      src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+      </script>
+    </div>
     <div id="map-legend">
       <a href="javascript:void(0);" class="fs-btn" title="Toggle Fullscreen"><i>Toggle Fullscreen</i></a>
       <a href="javascript:void(0);" class="mob-btn" title="Toggle Mobs"><i>Toggle Mobs</i></a>
@@ -27,4 +40,5 @@ wp_enqueue_script('dfuw_map',get_template_directory_uri().'/js/agon.map.js',fals
 </div>
 </div>
 </article>
+<div class="clear"></div>
 <?php get_footer(); ?>
