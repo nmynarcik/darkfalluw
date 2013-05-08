0da8bb7f881a1760b11bd34639902adcc488abb8
nmynarcik <nathan@mynarcik.com>
2013-05-08 15:19:46 -0500

    increase num of posts to even out page. update version to bust cache

--- page-darkfall.php
+++ page-darkfall.php
@@ -48,7 +48,7 @@ get_header();
               <p>Accumulation of all things Darkfall</p>
               <?php
                 $args = array(
-                                      'numberposts'     => 7,
+                                      'numberposts'     => 10,
                                       'category'        => get_category_by_slug('blog')->term_id,
                                       'orderby'         => 'post_date',
                                       'order'           => 'DESC',
