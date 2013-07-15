7494fb5b56258c5b2fe3baceb5a597639189559e
Nathan Mynarcik <nathan@mynarcik.com>
2013-07-15 17:20:20 -0500

    external prowess sheet went down. hosting our own

--- page-prowess.php
+++ page-prowess.php
@@ -10,7 +10,7 @@ get_header();
 <h1 class="entry-title"><?php the_title(); ?></h1>
 <div class="entry-content">
   <?php the_content(); ?>
-  <iframe class="gdoc" width="100%" height="750" src="https://docs.google.com/spreadsheet/lv?key=0Aj1bEOdIK16ldHJyQ2RGNTFKaVFqOHgtYXFzNGpsOHc&output=html"></iframe>
+  <iframe class="gdoc" width="100%" height="750" src="https://docs.google.com/spreadsheet/lv?key=0ArLeTh1m7WnrdFB5d3dUQmNjU01jOG1UZFRubVY0LWc&output=html"></iframe>
   <div class="disclaimer">** Content provided by Kelet</div>
 </div>
 </div>
