7538839ff23a4ed69332d406ff2163cad87f120c
Nathan Mynarcik <nathan@mynarcik.com>
2014-05-15 22:48:43 -0500

    this time, make sure the shit works :poop:

--- nav-below-single.php
+++ nav-below-single.php
@@ -1,4 +1,4 @@
 <div id="nav-below" class="navigation">
-<?php next_post_link(sprintf(__( '<div class="btn btn-inverse next">%s older</div>', 'darkfalluw' ),' <i class="icon-circle-arrow-left icon-white"></i>')) ?>
-<?php previous_post_link( sprintf(__( '<div class="btn btn-inverse prev">newer %s</div>', 'darkfalluw' ),'<i class="icon-circle-arrow-right icon-white"></i>')) ?>
+<?php next_post_link('%link',sprintf(__( '<div class="btn btn-inverse next">%s older</div>', 'darkfalluw' ),' <i class="icon-circle-arrow-left icon-white"></i>')) ?>
+<?php previous_post_link('%link',sprintf(__( '<div class="btn btn-inverse prev">newer %s</div>', 'darkfalluw' ),'<i class="icon-circle-arrow-right icon-white"></i>')) ?>
 </div>
