39c1a54a9472e6a2d4eaa53e73f30afb85edc56c
Nathan Mynarcik <nathan@mynarcik.com>
2014-06-11 02:50:45 -0500

    new 404 page

--- 404.php
+++ 404.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <div id="content" class="full-width">
 <div id="post-0" class="post error404 not-found">
-<h1 class="entry-title"><?php _e('4&bull;OH&bull;4', 'darkfalluw'); ?></h1>
-<div class="df-404"><img src="<?php echo get_template_directory_uri(); ?>/images/4OH4.jpg" /></div>
+<!-- <h1 class="entry-title"><?php _e('4&bull;OH&bull;4', 'darkfalluw'); ?></h1> -->
+<div class="df-404">It seems you have lost your way...</div>
 <div class="entry-content">
 <p><?php _e('Couldn\'t find anything at the page you requested. Try a search instead?', 'darkfalluw'); ?></p>
 <?php get_search_form(); ?>
