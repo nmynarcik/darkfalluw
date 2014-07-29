72ab9b0f7ecfab130f23cff30d560c8310c56a6b
Nathan Mynarcik <nathan@mynarcik.com>
2014-07-29 02:33:50 -0500

    remove commented scripts

--- page-crafting.php
+++ page-crafting.php
@@ -3,23 +3,8 @@
 Template Name: Crafting
 */
 get_header();
-// wp_enqueue_script('dfuw_jdrag', get_template_directory_uri().'/js/lib/jquery.event.drag-2.0.min.js',false,$er,'all');
-// wp_enqueue_script('dfuw_j2csv', get_template_directory_uri().'/js/lib/jquery.csv-0.71.min.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickcore', get_template_directory_uri().'/js/lib/slick.core.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickautotips', get_template_directory_uri().'/js/plugins/slick.autotooltips.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickcellranger', get_template_directory_uri().'/js/plugins/slick.cellrangeselector.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickcelldecor', get_template_directory_uri().'/js/plugins/slick.cellrangedecorator.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickcellext', get_template_directory_uri().'/js/plugins/slick.cellexternalcopymanager.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickcellselect', get_template_directory_uri().'/js/plugins/slick.cellselectionmodel.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickdata', get_template_directory_uri().'/js/lib/slick.dataview.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickeditor', get_template_directory_uri().'/js/lib/slick.editors.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickformat', get_template_directory_uri().'/js/lib/slick.formatters.js',false,$er,'all');
-// wp_enqueue_script('dfuw_slickgrid', get_template_directory_uri().'/js/slick.grid.js',false,$er,'all');
-// wp_enqueue_style( 'dfuw_slick_styles', get_template_directory_uri().'/css/slick.grid.css', false, $ver, 'all' );
 wp_enqueue_style( 'jquery_ui_css', get_template_directory_uri().'/css/jquery-ui.theme.min.css', false, $ver, 'all' );
-// wp_enqueue_script('jquery_ui_js', '//code.jquery.com/ui/1.11.0/jquery-ui.js',false,$ver,'all');
-// wp_enqueue_script('dfuw_crafting', get_template_directory_uri().'/js/crafting.js',false,$ver,'all'); //min
-wp_enqueue_script('dfuw_crafting', get_template_directory_uri().'/js/crafting.js', array('jquery','jquery-ui-js'),$ver,true); //min
+wp_enqueue_script('dfuw_crafting', get_template_directory_uri().'/js/crafting.min.js', array('jquery','jquery-ui-js'),$ver,true);
 wp_enqueue_script( $handle, $src, $deps, $ver, $in_footer );
 ?>
 <article id="content" class="full-width">
